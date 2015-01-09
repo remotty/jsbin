b316cdade292bde24fd77b7d36826c412a6baa04
toto <toto@smartstudy.co.kr>
2015-01-10 02:00:53 +0900

    Wrap files by module

--- public/js/chrome/esc.js
+++ public/js/chrome/esc.js
@@ -1,34 +1,52 @@
-var loginVisible = false,
-    dropdownOpen = false,
-    keyboardHelpVisible = false,
-    urlHelpVisible = false;
+var ESC = (function(){
+  var loginVisible = false;
+  var dropdownOpen = false;
+  var keyboardHelpVisible = false;
+  var urlHelpVisible = false;
 
-$document.keydown(function (event) {
-  if (event.which == 27) {//} || (keyboardHelpVisible && event.which == 191 && event.shiftKey && event.metaKey)) {
-    hideOpen();
+  var hideOpen = function() {
+    if (urlHelpVisible) {
+      $body.removeClass('urlHelp');
+      urlHelpVisible = false;
+      analytics.closeMenu('help');
+    } else if (keyboardHelpVisible) {
+      $body.removeClass('keyboardHelp');
+      keyboardHelpVisible = false;
+      analytics.closeMenu('keyboardHelp');
+    } else if (dropdownOpen) {
+      closedropdown();
+    } else if (loginVisible) {
+      $('#login').hide();
+      analytics.closeMenu('login');
+      loginVisible = false;
+    }
   }
-});
 
-function hideOpen() {
-  if (urlHelpVisible) {
-    $body.removeClass('urlHelp');
-    urlHelpVisible = false;
-    analytics.closeMenu('help');
-  } else if (keyboardHelpVisible) {
-    $body.removeClass('keyboardHelp');
-    keyboardHelpVisible = false;
-    analytics.closeMenu('keyboardHelp');
-  } else if (dropdownOpen) {
-    closedropdown();
-  } else if (loginVisible) {
-    $('#login').hide();
-    analytics.closeMenu('login');
-    loginVisible = false;
-  }
-}
+  var setup = function(){
+    $document.keydown(function (event) {
+      if (event.which == 27) {//} || (keyboardHelpVisible && event.which == 191 && event.shiftKey && event.metaKey)) {
+        hideOpen();
+      }
+    });
 
-$document.delegate('.modal', 'click', function (event) {
-  if ($(event.target).is('.modal')) {
-    hideOpen();
+    $document.delegate('.modal', 'click', function (event) {
+      if ($(event.target).is('.modal')) {
+        hideOpen();
+      }
+    });
   }
-});
+  return {
+    loginVisible: loginVisible,
+    dropdownOpen: dropdownOpen,
+    keyboardHelpVisible: keyboardHelpVisible,
+    urlHelpVisible: urlHelpVisible,
+    setup: setup
+  }
+})();
+
+var loginVisible = ESC.loginVisible;
+var dropdownOpen = ESC.dropdownOpen;
+var keyboardHelpVisible = ESC.keyboardHelpVisible;
+var urlHelpVisible = ESC.urlHelpVisible;
+
+ESC.setup();
