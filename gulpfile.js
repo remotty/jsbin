f47ffec7c6a98da943c36da9fa457cc652b00255
toto <toto@smartstudy.co.kr>
2015-01-22 20:07:43 +0900

    Add tasks(reload, test:server, serve)

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,11 @@
   'use strict';
 
   var gulp = require('gulp');
-  var gutil = require('gulp-util');
-  var webpack = require('webpack');
-  var gulpWebpack = require('gulp-webpack');  
-  var WebpackDevServer = require('webpack-dev-server');
+  var gulpWebpack = require('gulp-webpack');
+  var gulpConnect = require('gulp-connect');  
   var karma = require('gulp-karma');
   var webpackConfig = require('./webpack-config');
-  var watch = require('gulp-watch');
-  
-  var webpackDevServerPort = 6000;
+  var gulpMocha = require('gulp-mocha');
 
   var testFiles = [
     './test/client/hello.js',
@@ -27,6 +23,9 @@
         // Make sure failed tests cause gulp to exit non-zero
         throw err;
       });
+  gulp.task('test:server', function() {
+    return gulp.src('./test/server/**/*.js', {read: false})
+      .pipe(gulpMocha({reporter: 'nyan'}));
   });
 
   gulp.task('karma', function() {
@@ -43,7 +42,21 @@
       .pipe(gulp.dest('./public/js/prod'));
   });
 
+  gulp.task('serve', function(){
+    gulpConnect.server({
+      root: './public',
+      port: 8000,
+      livereload: true
+    });
+  });
+
+  gulp.task("reload", function() {
+    gulp.src(['./public/js/**/*.js', '!./public/js/prod/*.js'])
+      .pipe(gulpConnect.reload());
+  });
+  
   gulp.task('watch', function() {
-    gulp.watch(['./public/js/**/*.js', "!./public/js/prod/*.js"], ['build']);
+    gulp.watch(['./public/js/**/*.js', '!./public/js/prod/*.js'], ['build']);
+    gulp.watch(['./lib/**/*.js', './test/server/**/*.js'], ['test:server']);
   });
 })();
